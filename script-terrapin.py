a14436032917bcb7cb98665b17f3800bbc431ada
aptElliot <179106121+aptelliot@users.noreply.github.com>
2025-05-15 10:38:40 +0200

    Create script-terrapin.py

---
+++ script-terrapin.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# lanzo el script ,/script-terrapin.py y me pedirá agregar la ip y el usuario de la máquina a atacar
+read -p "Introduce la IP del servidor: " IP
+read -p "Introduce el nombre de usuario: " USER
+
+OUTPUT=$(ssh -vvv -oBatchMode=yes -oConnectTimeout=5 \
+    -oKexAlgorithms=curve25519-sha256 \
+    -oCiphers=chacha20-poly1305@openssh.com \
+    -oMACs=hmac-sha2-256-etm@openssh.com \
+    "$USER@$IP" exit 2>&1)
+
+if echo "$OUTPUT" | grep -q "no matching kex algorithm"; then
+    echo "[✔] NO vulnerable (rechaza Kex inseguro)"
+elif echo "$OUTPUT" | grep -q "no matching cipher"; then
+    echo "[✔] NO vulnerable (rechaza Cipher inseguro)"
+elif echo "$OUTPUT" | grep -q "no matching MAC found"; then
+    echo "[✔] NO vulnerable (rechaza MAC inseguro)"
+elif echo "$OUTPUT" | grep -q "Connection established"; then
+    echo "[!] VULNERABLE a Terrapin (algoritmos inseguros aceptados)"
+else
+    echo "[?] No se pudo determinar con certeza. Revisa manualmente:"
+    echo "$OUTPUT" | tail -n 10
+fi
