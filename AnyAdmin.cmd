774e1efcd0f9c6b379e3208eb95f582c73716d5b
aptElliot <179106121+aptelliot@users.noreply.github.com>
2025-05-23 11:11:22 +0200

    Update AnyAdmin.cmd

--- AnyAdmin.cmd
+++ AnyAdmin.cmd
@@ -7,7 +7,7 @@ setlocal
 
 :: Preguntar dominio
 echo Introduce el nombre del dominio 
-set /p dominio= (ej: APPLE): 
+set /p dominio= (ej: apple.com): 
 
 :: Preguntar ruta del archivo a ejecutar
 echo Introduce la ruta completa del archivo que quieres abrir "con comillas si tiene espacios" 
@@ -15,7 +15,7 @@ set /p ruta= (ej: "notepad.exe C:\Windows\System32\drivers\etc\hosts"):
 
 :: Preguntar usuario
 echo Introduce el nombre de usuario de Active Directory 
-set /p usuario= (alberto.fernandez): 
+set /p usuario= (craig.federighi): 
 
 :: Ejecutar con runas usando los datos introducidos
 C:\Windows\System32\runas /netonly /user:%dominio%\%usuario% %ruta%
