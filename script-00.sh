b912b731dceea7de8dd19c735c208ceda86ef84e
aptElliot <179106121+aptelliot@users.noreply.github.com>
2025-06-17 14:38:11 +0200

    Update script-00.sh

--- script-00.sh
+++ script-00.sh
@@ -22,7 +22,7 @@ apt-get install -y \
     iputils-ping lm-sensors iproute2 sudo vim net-tools curl btop iftop \
     lsb-release arping wget sysstat ntpdate snmp snmpd tcpdump \
     ngrep iptraf-ng mlocate tar gzip tree ca-certificates \
-    tmux man-db mailutils dnsutils telnet rsyslog  
+    screen man-db mailutils dnsutils telnet rsyslog  
 
 # Configuración de sensores
 echo "Configurando sensores:"
