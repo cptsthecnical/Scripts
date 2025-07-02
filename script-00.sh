4fbfe0a287aa517c3c7fe945c5efec2122ed6ec4
⚗️📖 root <179106121+isaac-laboratory-00@users.noreply.github.com>
2025-07-02 13:15:03 +0200

    Update script-00.sh

--- script-00.sh
+++ script-00.sh
@@ -112,9 +112,9 @@ alias df='df --exclude-type=tmpfs'
 ## Cambiar diseño del prompt (estilo cyberpunk)
 # **************************************
 # color 1
-PS1='\[\e[0;90m\]r00t箱\e[38;5;196m[\H]\e[38;5;196m\e[1;32m \w\e[0;37m $: '
+PS1='\[\e[0;90m\]\u箱\e[38;5;196m[\H]\e[38;5;196m\e[1;32m \w\e[0;37m $: '
 # color 2
-# PS1='\[\e[1;31m\]r00t箱[\H] \w $: \[\e[0m\]'
+# PS1='\[\e[1;31m\]\u箱[\H] \w $: \[\e[0m\]'
 
 ## cambiar colores para ls (estilo cyberpunk)
 # **************************************
