1a51b8c3546ff6d9ea8544a662bb7be8d2639387
⚗️📖 root <179106121+isaac-laboratory-00@users.noreply.github.com>
2025-07-18 09:44:19 +0200

    Update script-00.sh

--- script-00.sh
+++ script-00.sh
@@ -102,8 +102,8 @@ fi
 # Preguntar al usuario el estilo
 while true; do
     echo -e "${YELLOW}Selecciona el estilo: ${NC}"
-    echo "1) Estilo cyberpunk                        # VM y LXC"
-    echo "2) Estilo técnica/grisácea                 # PROXMOX"    
+    echo "1) Estilo cyberpunk                        # estilo cyberpunk (rosa y verde terminal)"
+    echo "2) Estilo técnica/grisácea                 # estilo alternativo (gris y amarillo)"    
     read -rp "Opción (1 o 2): " opcion
 
     case "$opcion" in
