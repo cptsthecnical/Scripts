3d4de204413ae4a4345a237689a1abe8c92e320b
cptsthecnical <179106121+cptsthecnical@users.noreply.github.com>
2025-10-29 13:20:57 +0100

    Create script-USB-tactico.bat

---
+++ WINDOWS/script-USB-tactico.bat
@@ -0,0 +1,49 @@
+@echo off
+setlocal enabledelayedexpansion
+
+## script para Windows en powershell para migrar con doble click mis apuntes públicos a un USB
+# Configuración
+set "DestinationFolder=E:\00-MANUALES"        # Cambiar por tu unidad USB
+set "GitHubToken=TOKEN"                       # Solo necesario para repos privados
+
+:: Crear carpeta de destino si no existe
+if not exist "%DestinationFolder%" (
+    mkdir "%DestinationFolder%"
+)
+
+# para generar un token en Settings > Developer Settings > Personal Access Token > Token (Classic)
+set "repos="public^|https://github.com/cptsthecnical/Scripts/zipball/main public^|https://github.com/cptsthecnical/Technical-Documentation/zipball/main private^|https://github.com/cptsthecnical/prueba-privada/zipball/main"  
+
+:: Descargar cada repo
+for %%u in (%repos%) do (
+    for /F "tokens=1,2 delims=^|" %%a in ("%%u") do (
+        set "repoType=%%a"
+        set "repoUrl=%%b"
+
+        :: Extraer nombre del repositorio
+        for /F "tokens=4 delims=/" %%c in ("!repoUrl!") do set "repoName=%%c"
+
+        set "filePath=%DestinationFolder%\!repoName!.zip"
+
+        echo =========================================
+        echo Preparando !repoName!.zip...
+
+        :: Intento de descarga
+        if "!repoType!"=="public" (
+            curl -L -o "!filePath!" "!repoUrl!" --silent
+        ) else (
+            curl -L -o "!filePath!" -H "Authorization: token %GitHubToken%" -H "User-Agent: curl" "!repoUrl!" --silent
+        )
+
+        :: Valido la descarga
+        if exist "!filePath!" (
+            echo [yes] - !repoName!.zip descargado en %DestinationFolder%
+        ) else (
+            echo [no] - Error al descargar !repoName!.zip
+        )
+    )
+)
+
+echo.
+echo Descarga finalizada.
+pause
